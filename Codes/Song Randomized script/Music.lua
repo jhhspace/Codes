9ac00f5abbc7d7f2f7c440322a25089fa43cc35f
jhhspace <105734516+jhhspace@users.noreply.github.com>
2022-05-19 01:24:32 +0800

    music

---
+++ Codes/Song Randomized script/Music.lua
@@ -0,0 +1,8 @@
+-- Here's what you need
+-- 1) Add a script
+-- 2) Rename the script to Music
+-- 3) Insert the script below into the script
+
+script.LocalBackgroundMusic.Parent = game.StarterPlayer.StarterPlayerScripts
+game.StarterPlayer.StarterPlayerScripts.LocalBackgroundMusic.Disabled = false
+script:remove()
